9369953fdd87de8730784bb220fdbf6d6b0313ef
Ryota Kawamura <dk.mat.18.of.a.challenge@gmail.com>
2017-02-28 11:53:30 +0900

    add members

--- savlog.py
+++ savlog.py
@@ -6,8 +6,10 @@ import urllib.request as req
 import argparse
 import sys, codecs
 import io
+import os
+import os.path
 
-global pageNum
+pageNum = 0
 
 def savlog_general(url):
     authors = []
@@ -30,9 +32,10 @@ def savlog_general(url):
         print ("name is %s and title is %s" % (authors[a].text, titles[a].text))
 
 
-def savlog_individual(url):
+def savlog_individual(url,member_path):
     titles = []
     bodies = []
+    global pageNum
 
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
@@ -53,11 +56,23 @@ def savlog_individual(url):
             sentence += one_sentence.text
         bodies.append(sentence)
 
+    if os.path.isdir(member_path) == False:
+        os.mkdir(member_path)
+
     if len(titles) == len(bodies):
         for num in range(len(titles)):
             print ("title is %s and content is %s" % (titles[num].text, bodies[num]))
+        pageNum += 1
     else:
         pass
+
+
+def return_url(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    request = req.Request(url=url, headers=headers)
+    html = req.urlopen(request).read().decode('utf-8')
+    soup = BeautifulSoup(html, 'lxml')
+    return soup
 
 
 if __name__ == '__main__':
@@ -65,20 +80,96 @@ if __name__ == '__main__':
     parser.add_argument('--name', '-n', type=str, default='')
     args = parser.parse_args()
 
-    if args.name == "橋本奈々未":
-        name = "nanami.hashimoto"
-    elif args.name == "西野七瀬":
-        name = "nanase.nishino"
-    elif args.name == "白石麻衣":
-        name = "mai.shiraishi"
-    elif args.name == "伊藤万理華":
-        name = "marika.ito"
-    elif args.name == "齋藤飛鳥":
-        name == "asuka.saito"
+    url = "http://blog.nogizaka46.com/"
 
-    url = "http://blog.nogizaka46.com/" + name
-    #savlog_general(url)
-    savlog_individual(url)
+    if args.name == '':
+        savlog_general(url)
+    else:
+        if args.name == "秋元真夏":
+            name = "manatsu.akimoto"
+        elif args.name == "生田絵梨花":
+            name = "erika.ikuta"
+        elif args.name == "生駒里奈":
+            name = "rina.ikoma"
+        elif args.name == "伊藤かりん":
+            name = "junna.itou"
+        elif args.name == "伊藤純奈":
+            name = "karin.itou"
+        elif args.name == "伊藤万理華":
+            name = "marika.ito"
+        elif args.name == "井上小百合":
+            name = "sayuri.inoue"
+        elif args.name == "衛藤美彩":
+            name = "misa.eto"
+        elif args.name == "川後陽菜":
+            name = "hina.kawago"
+        elif args.name == "川村真洋":
+            name = "mahiro.kawamura"
+        elif args.name == "北野日奈子":
+            name = "hinako.kitano"
+        elif args.name == "齋藤飛鳥":
+            name == "asuka.saito"
+        elif args.name == "斎藤ちはる":
+            name == "chiharu.saito"
+        elif args.name == "斉藤優里":
+            name == "yuuri.saito"
+        elif args.name == "相楽伊織":
+            name == "iori.sagara"
+        elif args.name == "桜井玲香":
+            name == "reika.sakurai"
+        elif args.name == "佐々木琴子":
+            name == "kotoko.sasaki"
+        elif args.name == "白石麻衣":
+            name = "mai.shiraishi"
+        elif args.name == "新内眞衣":
+            name == "mai.shinuchi"
+        elif args.name == "鈴木絢音":
+            name == "ayane.suzuki"
+        elif args.name == "高山一実":
+            name == "kazumi.takayama"
+        elif args.name == "寺田蘭世":
+            name == "ranze.terada"
+        elif args.name == "中田花奈":
+            name == "kana.nakada"
+        elif args.name == "中元日芽香":
+            name == "himeka.nakamoto"
+        elif args.name == "西野七瀬":
+            name = "nanase.nishino"
+        elif args.name == "能條愛未":
+            name == "ami.noujo"
+        elif args.name == "橋本奈々未":
+            name == "nanami.hashimoto"
+        elif args.name == "樋口日奈":
+            name == "hina.higuchi"
+        elif args.name == "星野みなみ":
+            name == "minami.hoshino"
+        elif args.name == "堀未央奈":
+            name == "miona.hori"
+        elif args.name == "松村沙友理":
+            name == "sayuri.matsumura"
+        elif args.name == "山崎怜奈":
+            name == "rena.yamazaki"
+        elif args.name == "若月佑美":
+            name == "yumi.wakatsuki"
+        elif args.name == "渡辺みり愛":
+            name == "miria.watanabe"
+        elif args.name == "和田まあや":
+            name == "maaya.wada"
+        elif args.name == "三期生":
+            name == "third"
+        else:
+            sys.stderr.write("There is no member! ")
 
-    #ページを増やして無限ループで関数呼び出し
-    #ページ数がこれ以上増えない(=pege not foundが返ってくる)時はbreak
+    member_path = './members/' + name
+    firstsearch_url = url + name
+
+    while True:
+        if pageNum == 0:
+            savlog_individual(firstsearch_url, member_path)
+        else:
+            new_name = name + "/?p=" + pageNum
+            nextsearch_url = url + new_name
+            if return_url(firstsearch_url) == return_url(nextsearch_url):
+                break
+            else:
+                savlog_individual(nextsearch_url, member_path)
