13fe539094d41fe9bbce68dd2aaf02057a3a2430
Ryota Kawamura <dk.mat.18.of.a.challenge@gmail.com>
2017-03-09 03:44:40 +0900

    enable to reserve some pages as txt and jpeg file

--- savlog.py
+++ savlog.py
@@ -3,6 +3,7 @@
 
 from bs4 import BeautifulSoup
 from PIL import Image
+from datetime import datetime
 import urllib.request as req
 import argparse
 import sys, codecs
@@ -11,7 +12,7 @@ import re
 import os
 import os.path
 
-pageNum = 0
+pageNum = 1
 
 
 def return_html(url):
@@ -22,11 +23,27 @@ def return_html(url):
     return soup
 
 
+def return_date(url):
+    _url = url
+    days = []
+    soup = return_html(_url)
+    entrybottom = soup.find_all('div', class_='entrybottom')
+    # get each date
+    for day in entrybottom:
+        split_word = day.text.split()
+        word = split_word[0]
+        split_day = word.split('/')
+        day1 = split_day[0]
+        day2 = split_day[1]
+        date = day1 + day2
+        days.append(date)
+
+    return days  
+
+
 def savlog_general(url):
     authors = []
     titles = []
-
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
     soup = return_html(url)
     spans = soup.find_all('span', class_ = 'heading')
@@ -46,12 +63,10 @@ def savlog_individual(url,member_path):
     images = []
     global pageNum
 
-    # make individual folder
+    # make individual member's folder or change current path to member path
     if not os.path.isdir(member_path):
+        #os.chmod("./members/", 0o777)
         os.mkdir(member_path)
-
-    # convert us-ascii to utf-8
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
     soup = return_html(url)
 
@@ -93,7 +108,10 @@ def savlog_individual(url,member_path):
             save_path = './' + days[num]
             if not os.path.isdir(save_path):
                 os.mkdir(save_path)
-            os.chdir(save_path)
+                os.chdir(save_path)
+            else:
+                os.chdir(save_path)
+            
             write_file = titles[num].text + '.txt'
             with open(write_file, 'w', encoding='utf-8') as f:
                 f.write(bodies[num])
@@ -110,9 +128,54 @@ def savlog_individual(url,member_path):
                 counter += 1
             os.chdir('../')
             #print (titles[num].text, bodies[num])
-        pageNum += 1
+
+    os.chdir('../../')
+
+
+def next_pages(url, search_url, name, member_path, year, month, date):
+    _url = url
+    _search_url = search_url
+    _name = name
+    _member_path = member_path
+    _year = year
+    _month = month
+    _date = date
+    global pageNum
+
+    new_name = _name + "/?p=" + str(pageNum) + "&d=" + _date
+    nextsearch_url = _url + new_name
+    
+    # page 2 doesn't exsits
+    counter = 0
+    days1 = return_date(_search_url)
+    days2 = return_date(nextsearch_url)
+    a = len(days2) if len(days1)>len(days2) else len(days1)
+    for i in range(a):
+        if days1[i] == days2[i]:
+            counter += 1
+
+    if counter == a:
+        pageNum = 1
+        calculated_month = int(_month) - 1
+
+        # move to previous month
+        if calculated_month == 0:
+            print ("next is previous year!")
+            _year = str(int(_year)-1)
+            _month = str(12)
+        elif calculated_month < 10:
+            print ("previous month!! still continuing!!")
+            _month = str(0) + str(calculated_month)
+        else:
+            print ("previous month")
+            _month = str(calculated_month)
+        return _year, _month
+            
+    # page 2 or more exists
     else:
-        pass
+        savlog_individual(nextsearch_url, _member_path)
+        pageNum += 1
+        return next_pages(_url, _search_url, _name, _member_path, _year, _month, _date)
 
 
 if __name__ == '__main__':
@@ -120,97 +183,138 @@ if __name__ == '__main__':
     parser.add_argument('--name', '-n', type=str, default='')
     args = parser.parse_args()
 
+    # convert us-ascii to utf-8
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
     url = "http://blog.nogizaka46.com/"
     name = ""
+    today = datetime.now()
+    year = today.strftime('%Y')
+    month = today.strftime('%m')
+    current_date = year + month
+    date = current_date
 
     if args.name == '':
         savlog_general(url)
     else:
         if args.name == "秋元真夏":
-            name = "manatsu.akimoto"
+            name = "manatsu.akimoto/"
         elif args.name == "生田絵梨花":
-            name = "erika.ikuta"
+            name = "erika.ikuta/"
         elif args.name == "生駒里奈":
-            name = "rina.ikoma"
+            name = "rina.ikoma/"
         elif args.name == "伊藤かりん":
-            name = "junna.itou"
+            name = "junna.itou/"
         elif args.name == "伊藤純奈":
-            name = "karin.itou"
+            name = "karin.itou/"
         elif args.name == "伊藤万理華":
-            name = "marika.ito"
+            name = "marika.ito/"
         elif args.name == "井上小百合":
-            name = "sayuri.inoue"
+            name = "sayuri.inoue/"
         elif args.name == "衛藤美彩":
-            name = "misa.eto"
+            name = "misa.eto/"
         elif args.name == "川後陽菜":
-            name = "hina.kawago"
+            name = "hina.kawago/"
         elif args.name == "川村真洋":
-            name = "mahiro.kawamura"
+            name = "mahiro.kawamura/"
         elif args.name == "北野日奈子":
-            name = "hinako.kitano"
+            name = "hinako.kitano/"
         elif args.name == "齋藤飛鳥":
-            name == "asuka.saito"
+            name == "asuka.saito/"
         elif args.name == "斎藤ちはる":
-            name == "chiharu.saito"
+            name == "chiharu.saito/"
         elif args.name == "斉藤優里":
-            name == "yuuri.saito"
+            name == "yuuri.saito/"
         elif args.name == "相楽伊織":
-            name == "iori.sagara"
+            name == "iori.sagara/"
         elif args.name == "桜井玲香":
-            name == "reika.sakurai"
+            name == "reika.sakurai/"
         elif args.name == "佐々木琴子":
-            name == "kotoko.sasaki"
+            name == "kotoko.sasaki/"
         elif args.name == "白石麻衣":
-            name = "mai.shiraishi"
+            name = "mai.shiraishi/"
         elif args.name == "新内眞衣":
-            name == "mai.shinuchi"
+            name == "mai.shinuchi/"
         elif args.name == "鈴木絢音":
-            name == "ayane.suzuki"
+            name == "ayane.suzuki/"
         elif args.name == "高山一実":
-            name == "kazumi.takayama"
+            name == "kazumi.takayama/"
         elif args.name == "寺田蘭世":
-            name == "ranze.terada"
+            name == "ranze.terada/"
         elif args.name == "中田花奈":
-            name == "kana.nakada"
+            name == "kana.nakada/"
         elif args.name == "中元日芽香":
-            name == "himeka.nakamoto"
+            name == "himeka.nakamoto/"
         elif args.name == "西野七瀬":
-            name = "nanase.nishino"
+            name = "nanase.nishino/"
         elif args.name == "能條愛未":
-            name == "ami.noujo"
+            name == "ami.noujo/"
         elif args.name == "橋本奈々未":
-            name == "nanami.hashimoto"
+            name == "nanami.hashimoto/"
         elif args.name == "樋口日奈":
-            name == "hina.higuchi"
+            name == "hina.higuchi/"
         elif args.name == "星野みなみ":
-            name == "minami.hoshino"
+            name == "minami.hoshino/"
         elif args.name == "堀未央奈":
-            name == "miona.hori"
+            name == "miona.hori/"
         elif args.name == "松村沙友理":
-            name == "sayuri.matsumura"
+            name == "sayuri.matsumura/"
         elif args.name == "山崎怜奈":
-            name == "rena.yamazaki"
+            name == "rena.yamazaki/"
         elif args.name == "若月佑美":
-            name == "yumi.wakatsuki"
+            name == "yumi.wakatsuki/"
         elif args.name == "渡辺みり愛":
-            name == "miria.watanabe"
+            name == "miria.watanabe/"
         elif args.name == "和田まあや":
-            name == "maaya.wada"
+            name == "maaya.wada/"
         elif args.name == "三期生":
-            name == "third"
+            name == "third/"
         else:
             sys.stderr.write("There is no member! ")
 
     member_path = './members/' + name
-    firstsearch_url = url + name
+    first_url = url + name
+    roop_counter = 1
 
     while True:
-        if pageNum == 0:
-            savlog_individual(firstsearch_url, member_path)
-        else:
-            new_name = name + "/?p=" + str(pageNum)
-            nextsearch_url = url + new_name
-            if return_html(firstsearch_url) == return_html(nextsearch_url):
+        print (date)
+        counter = 0
+        current_url = url + name + "?d=" + date
+        roop_counter += 1
+        first_url_days = return_date(first_url)
+        current_url_days = return_date(current_url)
+        for i in range(len(current_url_days)):
+            if first_url_days[i] == current_url_days[i]:
+                counter += 1
+
+        # this page is the same as first-url's page
+        if counter == len(first_url_days) == len(current_url_days):
+            counter = 0
+            # just in case
+            suspicious_month = str(int(month)-1)
+            suspicious_date = year + suspicious_month
+            suspicious_url = url + name + "?d=" + suspicious_date
+            suspicious_days = return_date(suspicious_url)
+            for i in range(len(suspicious_days)):
+                if first_url_days[i] == suspicious_days[i]:
+                    counter += 1
+            # not updated blog for two conecutive months -> there is no article cuz it is too previous year 
+            if counter == len(first_url_days) == len(suspicious_days):
+                print ('nothing!!!!')
                 break
-            else:
-                savlog_individual(nextsearch_url, member_path)
+
+        else:
+            print ('start')
+            #processing when page number is 1
+            search_url = url + name + "?d=" + date
+            savlog_individual(search_url, member_path)
+            pageNum += 1
+            print ('pagenum 0 finished!')
+
+            next_year, next_month = next_pages(url, search_url, name, member_path, year, month, date)
+            year = next_year
+            month = next_month
+            date = year + month
+            print ('round' + str(roop_counter) + 'finished!!')
+
+    print ('saving is finished!!')
